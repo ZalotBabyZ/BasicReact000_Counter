1ffa9ff0f5e7bba86a76b8bbc6519727903d3b98
ZalotBabyZ <babyzalot@gmail.com>
2020-11-01 16:14:05 +0700

    counter learn class state basic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    valueInput: "",
+    countN: 0,
+    disabled: true,
+  }
+  addNum = () => {
+    this.setState({
+      countN: ++this.state.countN,
+      disabled: this.state.countN <= 0
+    });
+  }
+  minusNum = () => {
+    this.setState({
+      countN: this.state.countN > 0 ? --this.state.countN : 0,
+      disabled: this.state.countN <= 0
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <input onChange={elem => this.setState({ valueInput: elem.target.value })} style={{ width: '300px' }} />
+        <h2>{this.state.valueInput}</h2>
+        <button onClick={this.addNum}> + </button>
+     &nbsp;&nbsp;&nbsp;{this.state.countN}&nbsp;&nbsp;&nbsp;
+        <button onClick={this.minusNum}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
